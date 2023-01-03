4997782ce50e5a106a1fc296a3a55ab9ff1c953e
Michael Kintscher <michael@kintscher.dev>
2023-01-03 09:26:55 -0700

    Add project files.

---
+++ Exploring AR Impact Paper Scripts/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exploring_AR_Impact_Paper_Scripts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
