715e227f164017125ba7b9fef65d408276bcb054
Michael Kintscher <michael@kintscher.dev>
2023-01-03 09:33:38 -0700

    Added the DataReader class and stubs for read methods.

---
+++ Exploring AR Impact Paper Scripts/DataReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exploring_AR_Impact_Paper_Scripts
+{
+    /// <summary>
+    /// Reads the input data. See README file for expected input formats.
+    /// </summary>
+    internal static class DataReader
+    {
+        /// <summary>
+        /// Reads the input data from the Looks table. See README file for expected input formats.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static void ReadLooksData()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads the input data from the Gestures table. See README file for expected input formats.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static void ReadGesturesData()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads the input data from the Utterances table. See README file for expected input formats.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static void ReadUtterancesData()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads the input data from the Utterances Count table. See README file for expected input formats.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static void ReadUtterancesCountData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
