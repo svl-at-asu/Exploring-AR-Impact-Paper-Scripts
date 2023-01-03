fe9fad9d5ef578b88e0d6fc4e98dac20926ab24b
Michael Kintscher <michael@kintscher.dev>
2023-01-03 11:23:23 -0700

    Prgram now reads in input data from command line args.

--- Exploring AR Impact Paper Scripts/Program.cs
+++ Exploring AR Impact Paper Scripts/Program.cs
@@ -8,8 +8,33 @@ namespace Exploring_AR_Impact_Paper_Scripts
 {
     internal class Program
     {
+        /// <summary>
+        /// Args are expected in the following order: Gestures, Looks, Utterances, Utterances Counts.
+        /// </summary>
+        private enum ArgsIndices
+        {
+            Gestures = 0,
+            Looks = 1,
+            Utterances = 2,
+            UtterancesCount = 3
+        }
+
+        /// <summary>
+        /// Expects a list of file names for the input data in the following order: Gestures, Looks, Utterances, Utterances Counts.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // Read the file data.
+            string[][] gesturesData;
+            try
+            {
+                gesturesData = DataReader.ReadCsvData(args[(int)Program.ArgsIndices.Gestures]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
