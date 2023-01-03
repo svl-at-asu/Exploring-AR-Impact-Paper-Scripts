f2bc55cdd2fdfd0c44e4f517d574617583e8a1a5
Michael Kintscher <michael@kintscher.dev>
2023-01-03 14:38:51 -0700

    Wrote Looks and Utterance data validation methods.

--- Exploring AR Impact Paper Scripts/DataValidator.cs
+++ Exploring AR Impact Paper Scripts/DataValidator.cs
@@ -14,6 +14,26 @@ namespace Exploring_AR_Impact_Paper_Scripts
     {
         #region Validation Data
         /// <summary>
+        /// The number of columns in the Gestures table.
+        /// </summary>
+        private static readonly int GesturesTableColumCount = 7;
+
+        /// <summary>
+        /// The number of columns in the Looks table.
+        /// </summary>
+        private static readonly int LooksTableColumCount = 4;
+
+        /// <summary>
+        /// The number of columns in the Utterances table.
+        /// </summary>
+        private static readonly int UtterancesTableColumCount = 7;
+
+        /// <summary>
+        /// The number of columns in the Utterances Count table.
+        /// </summary>
+        private static readonly int UtterancesCountTableColumCount = 8;
+
+        /// <summary>
         /// The number of teams.
         /// </summary>
         private static readonly int NumberOfTeams = 10;
@@ -28,6 +48,11 @@ namespace Exploring_AR_Impact_Paper_Scripts
             "Desktop2",
             "Hololens"
         };
+
+        /// <summary>
+        /// The format regex for time stamps.
+        /// </summary>
+        private static readonly string TimeStampFormat = @"[0-9]?[0-9]:[0-5][0-9]";
 
         /// <summary>
         /// The list of accepted gesture target values.
@@ -51,19 +76,22 @@ namespace Exploring_AR_Impact_Paper_Scripts
             "c"
         };
 
-        /// <summary>
-        /// The format regex for time stamps.
-        /// </summary>
-        private static readonly string TimeStampFormat = @"[0-9]?[0-9]:[0-5][0-9]";
+        private static readonly string[] UtterancePurposeValues =
+        {
+            "r",
+            "p",
+            "a",
+            "v"
+        };
         #endregion
 
+        #region Validation Methods
         /// <summary>
         /// Validates the gesture data. See README file for expected input formats.
         /// </summary>
         /// <param name="data">The gesture data in a 2D string array indexed by row then column.</param>
         /// <param name="issues">A list of issues found in the current data.</param>
         /// <returns>Whether the data is valid. The issues list is empty if true.</returns>
-        /// <exception cref="NotImplementedException"></exception>
         internal static bool ValidateGesturesData(string[][] data, out List<string> issues)
         {
             // Initialize the issues list.
@@ -77,6 +105,13 @@ namespace Exploring_AR_Impact_Paper_Scripts
 
                 // Set the issue header.
                 string issueHeader = "Gestures line " + lineNumber + " - ";
+
+                // Ensure the line has the correct number of columns.
+                if (DataValidator.ValidateLineLength(line, DataValidator.GesturesTableColumCount, ref issues, issueHeader) == false)
+                {
+                    // Move onto the next line, as no additional validation is possible for the current line.
+                    continue;
+                }
 
                 // Ensure the team number is a valid value.
                 DataValidator.ValidateTeamNumber(line[0], ref issues, issueHeader);
@@ -110,10 +145,42 @@ namespace Exploring_AR_Impact_Paper_Scripts
         /// <param name="data">The look data in a 2D string array indexed by row then column.</param>
         /// <param name="issues">A list of issues found in the current data.</param>
         /// <returns>Whether the data is valid. The issues list is empty if true.</returns>
-        /// <exception cref="NotImplementedException"></exception>
         internal static bool ValidateLooksData(string[][] data, out List<string> issues)
         {
-            throw new NotImplementedException();
+            // Initialize the issues list.
+            issues = new List<string>();
+
+            // For each line in the data...
+            for (int lineNumber = 0; lineNumber < data.Length; lineNumber++)
+            {
+                // Get a reference to the current line.
+                string[] line = data[lineNumber];
+
+                // Set the issue header.
+                string issueHeader = "Looks line " + lineNumber + " - ";
+
+                // Ensure the line has the correct number of columns.
+                if (DataValidator.ValidateLineLength(line, DataValidator.LooksTableColumCount, ref issues, issueHeader) == false)
+                {
+                    // Move onto the next line, as no additional validation is possible for the current line.
+                    continue;
+                }
+
+                // Ensure the team number is a valid value.
+                DataValidator.ValidateTeamNumber(line[0], ref issues, issueHeader);
+
+                // Ensure the video name is one of the accepted values.
+                DataValidator.ValidateVideoName(line[1], ref issues, issueHeader);
+
+                // Ensure the time stamp is formatted correctly.
+                DataValidator.ValidateTimeStamp(line[2], ref issues, issueHeader);
+
+                // Ensure the initiator is one of the accepted values.
+                DataValidator.ValidateIntegerInRange(line[3], 1, 2, "Initiator", ref issues, issueHeader);
+            }
+
+            // Return whether there are any issues.
+            return issues.Count == 0;
         }
 
         /// <summary>
@@ -122,10 +189,51 @@ namespace Exploring_AR_Impact_Paper_Scripts
         /// <param name="data">The utterance data in a 2D string array indexed by row then column.</param>
         /// <param name="issues">A list of issues found in the current data.</param>
         /// <returns>Whether the data is valid. The issues list is empty if true.</returns>
-        /// <exception cref="NotImplementedException"></exception>
         internal static bool ValidateUtterancesData(string[][] data, out List<string> issues)
         {
-            throw new NotImplementedException();
+            // Initialize the issues list.
+            issues = new List<string>();
+
+            // For each line in the data...
+            for (int lineNumber = 0; lineNumber < data.Length; lineNumber++)
+            {
+                // Get a reference to the current line.
+                string[] line = data[lineNumber];
+
+                // Set the issue header.
+                string issueHeader = "Utterances line " + lineNumber + " - ";
+
+                // Ensure the line has the correct number of columns.
+                if (DataValidator.ValidateLineLength(line, DataValidator.UtterancesTableColumCount, ref issues, issueHeader) == false)
+                {
+                    // Move onto the next line, as no additional validation is possible for the current line.
+                    continue;
+                }
+
+                // Ensure the team number is a valid value.
+                DataValidator.ValidateTeamNumber(line[0], ref issues, issueHeader);
+
+                // Ensure the video name is one of the accepted values.
+                DataValidator.ValidateVideoName(line[1], ref issues, issueHeader);
+
+                // Ensure the time stamp is formatted correctly.
+                DataValidator.ValidateTimeStamp(line[2], ref issues, issueHeader);
+
+                // Ensure the purpose is one of the accepted values.
+                DataValidator.ValidateEnum(line[3], DataValidator.UtterancePurposeValues, "Purpose", ref issues, issueHeader);
+
+                // Ensure the speaker is one of the accepted values.
+                DataValidator.ValidateIntegerInRange(line[4], 1, 2, "Speaker", ref issues, issueHeader);
+
+                // Ensure the deictic pronouns is one of the accepted values.
+                DataValidator.ValidateInteger(line[5], "Deictic Pronouns", ref issues, issueHeader);
+
+                // Ensure the spatial deictic is one of the accepted values.
+                DataValidator.ValidateInteger(line[5], "Spatial Deictic", ref issues, issueHeader);
+            }
+
+            // Return whether there are any issues.
+            return issues.Count == 0;
         }
 
         /// <summary>
@@ -139,8 +247,30 @@ namespace Exploring_AR_Impact_Paper_Scripts
         {
             throw new NotImplementedException();
         }
+        #endregion
 
         #region Helper Methods
+        /// <summary>
+        /// Ensures the line has the correct number of columns.
+        /// </summary>
+        /// <param name="line">The line to validate.</param>
+        /// <param name="numColumns">The number of columns the line should have.</param>
+        /// <param name="issues">A reference to the list of validation issues. New validation issues will be appended to this list.</param>
+        /// <param name="issueHeader">The standard header to add to the beginning of each new issue appended to the issues list.</param>
+        private static bool ValidateLineLength(string[] line,int numColumns, ref List<string> issues, string issueHeader)
+        {
+            // Ensure the line has the expected number of columns.
+            if (line.Count() != numColumns)
+            {
+                // The line does not have the expected number of columns. Add an issue to the list.
+                issues.Add(issueHeader + "Line does not have the expected number of values. Expected " + numColumns + " values.");
+                return false;
+            }
+
+            // If no issues were found, the issues list is unchanged.
+            return true;
+        }
+
         /// <summary>
         /// Ensures the team number is a valid integet between 1 and the Number of Teams.
         /// </summary>
@@ -222,6 +352,27 @@ namespace Exploring_AR_Impact_Paper_Scripts
             {
                 // The description is empty. Add an issue to the list.
                 issues.Add(issueHeader + "Gesture description is empty. Expected a non-empty description.");
+            }
+
+            // If no issues were found, the issues list is unchanged.
+        }
+
+        /// <summary>
+        /// Ensures the given value is a valid integer.
+        /// </summary>
+        /// <param name="dataString">The value to validate.</param>
+        /// <param name="valueName">The name of the value being validated (used in issue messages).</param>
+        /// <param name="issues">A reference to the list of validation issues. New validation issues will be appended to this list.</param>
+        /// <param name="issueHeader">The standard header to add to the beginning of each new issue appended to the issues list.</param>
+        private static void ValidateInteger(string dataString, string valueName, ref List<string> issues, string issueHeader)
+        {
+            // Ensure the data is an integer.
+            int data;
+            bool validInt = int.TryParse(dataString, out data);
+            if (validInt == false)
+            {
+                // The data is not a valid integer. Add an issue to the list.
+                issues.Add(issueHeader + valueName + " is not valid. Read: \"" + dataString + "\", expected an integer value.");
             }
 
             // If no issues were found, the issues list is unchanged.
