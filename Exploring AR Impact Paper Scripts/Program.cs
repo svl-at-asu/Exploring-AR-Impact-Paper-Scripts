7f087c48c71c213724c66ea2bc8c8b6a310b503f
Michael Kintscher <michael@kintscher.dev>
2023-01-04 01:32:06 -0700

    Finished implementing Main program.

--- Exploring AR Impact Paper Scripts/Program.cs
+++ Exploring AR Impact Paper Scripts/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,31 +10,130 @@ namespace Exploring_AR_Impact_Paper_Scripts
     internal class Program
     {
         /// <summary>
-        /// Args are expected in the following order: Gestures, Looks, Utterances, Utterances Counts.
+        /// Args are expected in the following order: Gestures, Looks, Utterances, Utterances Counts, Output.
         /// </summary>
         private enum ArgsIndices
         {
             Gestures = 0,
             Looks = 1,
             Utterances = 2,
-            UtterancesCount = 3
+            UtterancesCount = 3,
+            Output = 4
         }
 
+        private delegate bool ValidateTableHandler(string[][] data, out List<string> issues);
+
         /// <summary>
-        /// Expects a list of file names for the input data in the following order: Gestures, Looks, Utterances, Utterances Counts.
+        /// Expects a list of file names for the input data in the following order: Gestures, Looks, Utterances, Utterances Counts, Output.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // Read the file data.
-            string[][] gesturesData;
+            // Initialize the sets of issues.
+            Dictionary<string, List<string>> issues = new Dictionary<string, List<string>>()
+            {
+                { "Gestures", new List<string>() },
+                { "Looks", new List<string>() },
+                { "Utterances", new List<string>() },
+                { "Utterances Count", new List<string>() }
+            };
+
+            // Validate the Gestures table.
+            ValidateTableHandler validateGestures = DataValidator.ValidateGesturesData;
+            Program.ValidateTable(args[(int)Program.ArgsIndices.Gestures], "Gestures", validateGestures, out List<string> gesturesIssues);
+            issues["Gestures"] = gesturesIssues;
+
+            // Validate the Looks table.
+            ValidateTableHandler validateLooks = DataValidator.ValidateLooksData;
+            Program.ValidateTable(args[(int)Program.ArgsIndices.Looks], "Looks", validateLooks, out List<string> looksIssues);
+            issues["Looks"] = looksIssues;
+
+            // Validate the Utterances table.
+            ValidateTableHandler validateUtterances = DataValidator.ValidateUtterancesData;
+            Program.ValidateTable(args[(int)Program.ArgsIndices.Utterances], "Utterances", validateUtterances, out List<string> utterancesIssues);
+            issues["Utterances"] = utterancesIssues;
+
+            // Validate the Utterances Count table.
+            ValidateTableHandler validateUtterancesCount = DataValidator.ValidateUtterancesCountData;
+            Program.ValidateTable(args[(int)Program.ArgsIndices.UtterancesCount], "Utterances Count", validateUtterancesCount, out List<string> utterancesCountIssues);
+            issues["Utterances Count"] = utterancesCountIssues;
+
+            // Generate the issues log.
+            Program.GenerateOutputLog(args[(int)Program.ArgsIndices.Output], issues);
+        }
+
+        /// <summary>
+        /// Validates the data in the table CSV.
+        /// </summary>
+        /// <param name="filePath">The file path to the table file.</param>
+        /// <param name="issues">A list of issues found in the current data.</param>
+        private static void ValidateTable(string filePath, string tableName, ValidateTableHandler validationMethod, out List<string> issues)
+        {
+            // Initialize the issues list.
+            issues = new List<string>();
+
+            // Read the table file data.
+            string[][] data = null;
             try
             {
-                gesturesData = DataReader.ReadCsvData(args[(int)Program.ArgsIndices.Gestures]);
+                data = DataReader.ReadCsvData(filePath);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                // An error occured while trying to read the file data. Add an issue to the list.
+                issues.Add(tableName + " table error - " + ex.Message);
+
+                // Return, as no data validation is possible.
+                return;
+            }
+
+            // Validate the table file data.
+            validationMethod(data, out issues);
+        }
+
+        /// <summary>
+        /// Generates an output log of the given issues at the given path.
+        /// </summary>
+        /// <param name="outputFilePath">The path to generate the log at.</param>
+        /// <param name="allIssues">A set of issue lists for each data table.</param>
+        private static void GenerateOutputLog(string outputFilePath, Dictionary<string, List<string>> allIssues)
+        {
+            // Create the file.
+            using (FileStream stream = File.Create(outputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    // Write the header.
+                    writer.WriteLine("ISSUES LOG");
+                    writer.WriteLine();
+                    writer.WriteLine();
+
+                    foreach (KeyValuePair<string, List<string>> item in allIssues)
+                    {
+                        // Write the header.
+                        writer.WriteLine(item.Key);
+                        writer.WriteLine();
+
+                        // Write the issues.
+                        if (allIssues[item.Key].Count == 0)
+                        {
+                            writer.WriteLine("No issues found!");
+                        }
+                        else
+                        {
+                            foreach (string issue in allIssues[item.Key])
+                            {
+                                writer.WriteLine(issue);
+                            }
+                        }
+
+                        writer.WriteLine();
+                        writer.WriteLine();
+                    }
+
+                    // Write the end of the log.
+                    writer.WriteLine("END OF LOG");
+                }
             }
         }
     }
